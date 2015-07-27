4764da8ea70cd51c650c5936f9746308fef2e02c
JeramyHarrell <jharrell_@outlook.com>
2015-07-26 20:04:22 -0500

    description string

--- engine/room.go
+++ engine/room.go
@@ -1,33 +1,32 @@
 package engine
 
-import (
-
-)
+import ()
 
 type Room struct {
-  ID       int
-  Name     string
-  Realm    string
-  Position Position
-  Active   bool
+	ID          int
+	Name        string
+	Realm       string
+	Position    Position
+	Active      bool
+	Description string
 }
 
 func (room *Room) Init() {
-  room.Deactivate()
+	room.Deactivate()
 }
 
 func (room *Room) Update() {
-  // do something, this is the "slow update", world-level update
+	// do something, this is the "slow update", world-level update
 }
 
 func (room *Room) Activate() {
-  room.Active = true
-  // here we should activate the "fast updates" for entities in the room
-  // spin those off into separate goroutines
+	room.Active = true
+	// here we should activate the "fast updates" for entities in the room
+	// spin those off into separate goroutines
 }
 
 func (room *Room) Deactivate() {
-  room.Active = false
+	room.Active = false
 
-  // we should now "cool down" the "fast updates"
+	// we should now "cool down" the "fast updates"
 }
