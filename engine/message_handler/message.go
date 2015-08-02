fb66f6063bd730b7ff1de80d19c80a5608e22ef8
Jonathon Harrell <kaos78414@yahoo.com>
2015-08-01 22:34:25 -0500

    Messages parsing and sending to methods

--- engine/message_handler/message.go
+++ engine/message_handler/message.go
@@ -2,30 +2,42 @@
 package message_handler
 
 import (
-	"bytes"
 	"github.com/jonathonharrell/miri-ws-server/engine/websocket"
 	"reflect"
-	"strings"
+	"fmt"
 )
 
+type Methods struct {}
+var m = &Methods{}
+
+func (m *Methods) Say(args ...interface{}) {
+	fmt.Printf("%v\n", args)
+}
+
 func Route(name string, args ...interface{}) {
-	var m *websocket.Message
 	if len(args) > 0 {
 		inputs := make([]reflect.Value, len(args))
 		for i, _ := range args {
 			inputs[i] = reflect.ValueOf(args[i])
 		}
 		reflect.ValueOf(m).MethodByName(name).Call(inputs)
-	} else if len(args) == 0 {
-		reflect.ValueOf(m).MethodByName(name).Call([]reflect.Value{})
-	} else if reflect.ValueOf(m).NumField() == 0 {
-		// reflect.ValueOf(m).MethodByName("Say").Call(reflect.Value{"That isn't a command!"})
-	}
+	}// else if len(args) == 0 {
+	// 	reflect.ValueOf(m).MethodByName(name).Call([]reflect.Value{})
+	// } else if reflect.ValueOf(m).NumField() == 0 {
+	// 	// reflect.ValueOf(m).MethodByName("Say").Call(reflect.Value{"That isn't a command!"})
+	// }
 }
 
-func Interpreter(a *websocket.Message) {
-	n := bytes.Index(a.Payload, []byte{0})
-	str := string(a.Payload[:n])
-	firstWord := strings.Fields(str)
-	Route(firstWord[0], str[1:n])
+func Interpreter(m *websocket.Message) {
+	var n int
+	for i := 0; i < len(m.Payload); i++ {
+		if string(m.Payload[i]) == " " {
+			n = i
+			break
+		}
+	}
+
+	cmd := string(m.Payload[0:n])
+	args := string(m.Payload[(n + 1):len(m.Payload)])
+	Route(cmd, args)
 }
