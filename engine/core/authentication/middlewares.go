1e03ce850fe282cf0a90e364548a611a5260c6c5
Jonathon Harrell <jonathonharrell@yahoo.com>
2015-10-13 13:52:34 -0500

    Remove unneeded logging

--- engine/core/authentication/middlewares.go
+++ engine/core/authentication/middlewares.go
@@ -12,16 +12,11 @@ func RequireTokenAuthentication(rw http.ResponseWriter, req *http.Request, next 
 
 	token, err := jwt.ParseFromRequest(req, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			logger.Write.Info("Unexpected signing method")
 			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
 		} else {
-			logger.Write.Info("Token method is what we expect")
 			return authBackend.Key, nil
 		}
 	})
-
-	logger.Write.Info("%v", err)
-	logger.Write.Info("%v", token)
 
 	if err == nil && token.Valid && !authBackend.IsInBlacklist(req.Header.Get("Authorization")) {
 		next(rw, req)
