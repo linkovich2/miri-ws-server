93b1e3913021aa49a16f0a369614838a7b561aa8
Jonathon Harrell <jonathonharrell@yahoo.com>
2015-11-20 14:57:43 -0600

    Add some new backgrounds

--- app/content/backgrounds.go
+++ app/content/backgrounds.go
@@ -5,10 +5,28 @@ import (
 )
 
 var Backgrounds = map[string]core.Background{
-	"NOMADDESERT": core.Background{
-		Name:        "Nomad - Kai Ara",
-		ID:          "NOMADDESERT",
-		Description: "You are a nomad, wandering the Ara Wastes.",
+	"NOMADKAIARA": core.Background{
+		Name:        "Nomad - Tulin Desert",
+		ID:          "NOMADKAIARA",
+		Description: "You are a nomad, currently traveling through the Tulin Desert.",
+		AllowAll:    true,
+	},
+	"NOMADELDOREI": core.Background{
+		Name:        "Nomad - Sapphire Coast",
+		ID:          "NOMADELDOREI",
+		Description: "You are a nomad, currently traveling through the Sapphire Coast.",
+		AllowAll:    true,
+	},
+	"NOMADFOXEAR": core.Background{
+		Name:        "Nomad - Soro Fields",
+		ID:          "NOMADFOXEAR",
+		Description: "You are a nomad, currently traveling through the Soro Fields.",
+		AllowAll:    true,
+	},
+	"NOMADBRIREE": core.Background{
+		Name:        "Nomad - Skyshroud Forest",
+		ID:          "NOMADBRIREE",
+		Description: "You are a nomad, currently traveling through the Skyshround Forest.",
 		AllowAll:    true,
 	},
 }
