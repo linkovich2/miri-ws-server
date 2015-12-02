2d796490f9cdcb2d25874d43195b59c8d9d33a20
Jonathon Harrell <jonathonharrell@yahoo.com>
2015-12-02 16:29:20 -0600

    Validate functional traits

--- app/game/characters/controller.go
+++ app/game/characters/controller.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"github.com/jonathonharrell/miri-ws-server/app/content"
 	"github.com/jonathonharrell/miri-ws-server/app/core"
+	"strconv"
 	// "github.com/jonathonharrell/miri-ws-server/app/database"
 	"github.com/jonathonharrell/miri-ws-server/app/game"
 	"github.com/jonathonharrell/miri-ws-server/app/logger"
@@ -195,8 +196,86 @@ func validateAestheticTraits(connection *game.Connection, character *core.Charac
 
 func validateFunctionalTraits(connection *game.Connection, character *core.Character) (valid bool) {
 	// @todo this should look the same as aesthetic trait validation except individual traits may be required within a category
-	// and we also need to validate the point total is positive
-	return
+	var points int
+
+	for key, traits := range character.FunctionalTraits {
+		if _, categoryExists := content.FunctionalTraits[key]; !categoryExists { // trait category does not exist
+			logger.Write.Error("Character Creation Error (Connection [%s]): Unknown Trait Category [%s].", connection.Socket.ID, key)
+			return false
+		}
+		category := content.FunctionalTraits[key]
+
+		if !category.IsAllowedForCharacter(character) && len(traits) > 0 { // not allowed for character and one exists
+			logger.Write.Error("Character Creation Error (Connection [%s]): Trait Category [%s] not allowed for character.", connection.Socket.ID, key)
+			return false
+		}
+
+		if len(traits) > 1 && category.Unique { // character contains more then one trait in a unique category
+			logger.Write.Error("Character Creation Error (Connection [%s]): Trait Category [%s] is unique.", connection.Socket.ID, key)
+			return false
+		}
+
+		for _, trait := range traits {
+			if _, traitExists := category.Traits[trait]; !traitExists { // trait itself does not exist
+				logger.Write.Error("Character Creation Error (Connection [%s]): Trait [%s] does not exist.", connection.Socket.ID, trait)
+				return false
+			}
+
+			t := category.Traits[trait]
+			pointValue, err := strconv.Atoi(t.Points)
+
+			if err != nil {
+				logger.Write.Error("Weird conversion for point value for functional trait [%s]", t.ID)
+				return false
+			}
+
+			points = points + pointValue
+
+			if !t.IsAllowedForCharacter(character) { // individual trait is not allowed for this character
+				logger.Write.Error("Character Creation Error (Connection [%s]): Trait [%s] is not allowed for character.", connection.Socket.ID, trait)
+				return false
+			}
+		}
+	}
+
+	if points > 0 {
+		logger.Write.Error("Character Creation Error (Connection [%s]): Point deficit too high.", connection.Socket.ID)
+		return false
+	}
+
+	// loop through all aesthetic trait categories and check if one exists where one is required
+	for key, category := range content.FunctionalTraits {
+		if category.Minimum > 0 && category.IsAllowedForCharacter(character) {
+			if len(character.FunctionalTraits[key]) < category.Minimum {
+				logger.Write.Error("Character Creation Error (Connection [%s]): Character doesn't have enough traits from Trait Category [%s]", connection.Socket.ID, key)
+				return false
+			}
+		}
+
+		for id, trait := range category.Traits {
+			if trait.Required {
+				if _, categoryExists := character.FunctionalTraits[key]; !categoryExists {
+					logger.Write.Error("Character Creation Error (Connection [%s]): Required Trait Category [%s] doesn't exist.", connection.Socket.ID, key)
+					return false
+				}
+
+				exists := false
+				for _, t := range character.FunctionalTraits[key] {
+					if t == id {
+						exists = true
+						break
+					}
+				}
+
+				if !exists { // trait is required but doesn't exist on the character object
+					logger.Write.Error("Character Creation Error (Connection [%s]): Required Trait [%s] doesn't exist.", connection.Socket.ID, trait)
+					return false
+				}
+			}
+		}
+	}
+
+	return true
 }
 
 func validateBackground(connection *game.Connection, character *core.Character) (valid bool) {
