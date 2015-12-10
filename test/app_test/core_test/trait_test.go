7413e8f7ecf63b645f5541092c238b3487a147ad
Jonathon Harrell <jonathonharrell@yahoo.com>
2015-12-10 10:04:32 -0600

    Add test for trait validation

--- test/app_test/core_test/trait_test.go
+++ test/app_test/core_test/trait_test.go
@@ -14,5 +14,29 @@ var (
 )
 
 func TestTraitValidateCharacterAgainst(t *testing.T) {
-	// pass := &Character{}
+	onlyPass := &Character{Race: "TEST"}
+	onlyFail := &Character{Race: "NOPE"}
+	racePass := onlyFail
+	raceFail := onlyPass
+	genderPass := &Character{Race: "T", Gender: "F"}
+	genderFail := &Character{Race: "T", Gender: "T"}
+
+	if !testTraitOnly.IsAllowedForCharacter(onlyPass) {
+		t.Error("Only should have passed")
+	}
+	if testTraitOnly.IsAllowedForCharacter(onlyFail) {
+		t.Error("Only should have failed")
+	}
+	if !testTrait.IsAllowedForCharacter(genderPass) {
+		t.Error("Gender should have passed")
+	}
+	if testTrait.IsAllowedForCharacter(genderFail) {
+		t.Error("Gender should have failed")
+	}
+	if !testTrait.IsAllowedForCharacter(racePass) {
+		t.Error("Race should have passed")
+	}
+	if testTrait.IsAllowedForCharacter(raceFail) {
+		t.Error("Race should have failed")
+	}
 }
