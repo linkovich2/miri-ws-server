d413769eb7e447129e5a962ba951474f3175113a
Jonathon Harrell <jonathonharrell@yahoo.com>
2015-12-23 10:47:02 -0600

    Begin to detail some of the rooms around the tulinne desert starting location

--- app/content/world/rooms/miri.go
+++ app/content/world/rooms/miri.go
@@ -5,29 +5,53 @@ import (
 )
 
 var Miri = map[string]*core.Room{
-	"0:0:1": &core.Room{
-		Position:    core.Position{0, 0, 1},
-		Name:        "Kra Iree'a Outskirts",
-		Description: "Description @todo heyyyyy",
-		Size:        core.RoomSmall,
+	"1:0:1": &core.Room{
+		Position:    core.Position{1, 0, 1},
+		Name:        "Kra Jafic Traders' Outpost - Southern Bazaar",
+		Description: "The edge of the bazaar - merchants come and go among the tents bartering goods and services. Bright orange and blue tents match the local dress. There is such a variety of smells from so many different locales - some pleasant, others foul. The chatter of trading crowds is oddly comforting.",
+		Size:        core.RoomLarge,
+		Details:     []string{"trading tents", "bazaar", "sand", "traders"},
+	},
+	"0:1:1": &core.Room{
+		Position:    core.Position{0, 1, 1},
+		Name:        "Kra Jafic Traders' Outpost - Eastern Bazaar",
+		Description: "The eastern area of this trading center seems quiet, but traders still come and go. There isn't a lot on display here - the trading must be going on inside the orange and blue tents.",
+		Size:        core.RoomLarge,
+		Details:     []string{"trading tents", "bazaar", "sand", "traders"},
 	},
 	"1:1:1": &core.Room{
 		Position:    core.Position{1, 1, 1},
-		Name:        "Kra Iree'a",
-		Description: "Orange, blue and gold tents line the edges of this traders hub on the northern rim of the Tulinne Desert. Traders seem to come and go at odd times. The air is hot and dry, and the wind stirs the sands.",
+		Name:        "Kra Jafic Traders' Outpost",
+		Description: "Orange, blue and gold tents line the edges of this traders' hub on the northern rim of the Tulinne Desert. Traders seem to come and go at odd times. The air is hot and dry, and the wind stirs the sands.",
 		Size:        core.RoomLarge,
-		Details:     []string{"tents", "sand", "traders"},
+		Details:     []string{"trading tents", "bazaar", "sand", "traders"},
 	},
 	"1:2:1": &core.Room{
 		Position:    core.Position{1, 2, 1},
-		Name:        "Kra Iree'a Outskirts",
-		Description: "Description stuff @todo.",
-		Size:        core.RoomSmall,
+		Name:        "Kra Jafic Traders' Outpost - Northern Edge",
+		Description: "The tents of Kra Jafic are to the south, but at this edge of the outpost it is quiet. Here is where the stone road is no longer maintained, and the harsh desert surrounding this place seems endless.",
+		Size:        core.RoomLarge,
+		Details:     []string{"sand", "paved stone"},
 	},
-	"2:3:1": &core.Room{
-		Position:    core.Position{2, 3, 1},
-		Name:        "BIG TODO",
-		Description: "Description stuff @todo.",
-		Size:        core.RoomMedium,
+	"2:2:1": &core.Room{
+		Position:    core.Position{2, 2, 1},
+		Name:        "Kirenov Sands",
+		Description: "To the north you can feintly make out the outline of the Kai Era mountains.",
+		Size:        core.RoomMassive,
+		Details:     []string{"sand"},
+	},
+	"2:1:1": &core.Room{
+		Position:    core.Position{2, 1, 1},
+		Name:        "Kirenov Sands",
+		Description: "The sand seems to go on forever.",
+		Size:        core.RoomMassive,
+		Details:     []string{"sand"},
+	},
+	"1:-1:1": &core.Room{
+		Position:    core.Position{1, -1, 1},
+		Name:        "Kirenov Sands",
+		Description: "This desert seems to go on forever.",
+		Size:        core.RoomMassive,
+		Details:     []string{"sand"},
 	},
 }
